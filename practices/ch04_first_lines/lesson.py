980e98b0b0713b725561d876a9212978fc28b9da
Dan Brown <dan@lantzer.org>
2020-07-02 18:42:52 -0400

    Initial

---
+++ practices/ch04_first_lines/lesson.py
@@ -0,0 +1,23 @@
+import datetime
+
+myage = 51
+
+datenow = datetime.datetime.now()
+year = datenow.year
+
+print(f"The year is now : {year}")
+
+name = input('What is your name? ')
+age_t = input('How old are you? ')
+
+age = int(age_t)
+
+if age < myage:             # I am the eldest
+    print(f'I am {myage - age} years older than you {name}!')
+else:
+    if age == myage:        # We are same age
+        print(f'Well {name}, we are the same age of {age}!')
+    else:                   # I am the youngest!
+        print(f'I am {age - myage} years younger than you {name}!')
+
+print("Isn't Python is great!!!")
